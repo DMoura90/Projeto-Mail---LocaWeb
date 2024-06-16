1cff393759691130b99ae294fe51643fb539ce0e
Thomas Jefferson <rm98285@fiap.com.br>
2024-06-15 23:04:43 -0300

    Acerto de responsividade da tela

--- app/src/main/java/br/com/fiap/monitoramentodetemperatura/screens/NovoEmailScreen.kt
+++ app/src/main/java/br/com/fiap/monitoramentodetemperatura/screens/NovoEmailScreen.kt
@@ -7,10 +7,12 @@ import android.os.Vibrator
 import androidx.annotation.Size
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -39,6 +41,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -55,8 +58,9 @@ import androidx.navigation.compose.rememberNavController
 import br.com.fiap.monitoramentodetemperatura.R
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
+
 @Composable
-fun NovoEmailScreen(controleGeral: NavController){
+fun NovoEmailScreen(controleGeral: NavController) {
 
     fun vibratePhone(context: Context) {
         val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
@@ -66,6 +70,7 @@ fun NovoEmailScreen(controleGeral: NavController){
             vibrator.vibrate(100)
         }
     }
+
     val context = LocalContext.current
 
     var De by remember() {
@@ -74,28 +79,30 @@ fun NovoEmailScreen(controleGeral: NavController){
     var Para by remember() {
         mutableStateOf("")
     }
-    var Titulo by remember(){
+    var Titulo by remember() {
         mutableStateOf("")
     }
-    var ConteudoEmail by remember(){
+    var ConteudoEmail by remember() {
         mutableStateOf("")
     }
 
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color(0xFF121212))
-//        .padding(32.dp)
-    ){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF121212))
+    ) {
         Column(
-            modifier = Modifier.padding(16.dp)
-        ){
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
             Spacer(modifier = Modifier.height(20.dp))
 
             IconButton(
                 onClick = {
                     vibratePhone(context)
                     controleGeral.navigate("Emails")
-                          },
+                },
                 modifier = Modifier.size(30.dp)
             ) {
                 Icon(
@@ -103,7 +110,7 @@ fun NovoEmailScreen(controleGeral: NavController){
                     contentDescription = "Ícone voltar",
                     modifier = Modifier.size(30.dp),
 
-                )
+                    )
             }
 
             Spacer(modifier = Modifier.height(20.dp))
@@ -111,17 +118,19 @@ fun NovoEmailScreen(controleGeral: NavController){
             OutlinedTextField(
                 value = De,
                 onValueChange = {
-                    De = it },
+                    De = it
+                },
                 modifier = Modifier.fillMaxWidth(),
                 shape = RoundedCornerShape(30.dp),
                 colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = Color.LightGray,
-                    unfocusedBorderColor = Color.Gray),
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.LightGray
+                ),
                 placeholder = {
                     Text(text = "Seu E-mail")
                 },
                 label = {
-                    Text (text = "De:")
+                    Text(text = "De:")
                 },
                 trailingIcon = {
                     IconButton(
@@ -146,14 +155,14 @@ fun NovoEmailScreen(controleGeral: NavController){
                 modifier = Modifier.fillMaxWidth(),
                 shape = RoundedCornerShape(30.dp),
                 colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = Color.LightGray,
-                    unfocusedBorderColor = Color.Gray
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.LightGray
                 ),
                 placeholder = {
                     Text(text = "E-mail do destinatário")
                 },
                 label = {
-                    Text (text = "para:")
+                    Text(text = "para:")
                 },
                 trailingIcon = {
                     IconButton(
@@ -178,14 +187,14 @@ fun NovoEmailScreen(controleGeral: NavController){
                 modifier = Modifier.fillMaxWidth(),
                 shape = RoundedCornerShape(30.dp),
                 colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = Color.LightGray,
-                    unfocusedBorderColor = Color.Gray
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.LightGray
                 ),
                 placeholder = {
                     Text(text = "Assunto do E-mail")
                 },
                 label = {
-                    Text (text = "Título: ")
+                    Text(text = "Título: ")
                 },
                 trailingIcon = {
                     IconButton(
@@ -203,92 +212,90 @@ fun NovoEmailScreen(controleGeral: NavController){
 
             Spacer(modifier = Modifier.height(10.dp))
 
-            OutlinedTextField(
-                value = ConteudoEmail,
-                onValueChange = {
-                    ConteudoEmail = it
-                },
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(400.dp),
-                shape = RoundedCornerShape(30.dp),
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = Color.LightGray,
-                    unfocusedBorderColor = Color.Gray
-                ),
-                placeholder = {
-                    Text(text = "Conteúdo do E-mail")
-                },
-                label = {
-                    Text (text = "Escreva... ")
-                },
-            )
-
-            Spacer(modifier = Modifier.height(28.dp))
-
-        Row(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Button(
-                onClick = { vibratePhone(context) },
-                colors = ButtonDefaults.buttonColors(Color(0xff343434)),
-                modifier = Modifier
-                    .width(160.dp)
-                    .height(50.dp)
-                    .offset(x = 25.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceBetween
             ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_settings_24),
-                    contentDescription = "Ícone de configurações",
+
+                OutlinedTextField(
+                    value = ConteudoEmail,
+                    onValueChange = {
+                        ConteudoEmail = it
+                    },
                     modifier = Modifier
-                        .size(30.dp),
-                    tint = Color.LightGray
-
+                        .fillMaxWidth()
+                        .weight(1f),
+                    shape = RoundedCornerShape(30.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.LightGray
+                    ),
+                    placeholder = {
+                        Text(text = "Conteúdo do E-mail")
+                    },
+                    label = {
+                        Text(text = "Escreva... ")
+                    },
                 )
-                Text(
-                    text = "Opções",
-                    fontSize = 20.sp,
-                    color = Color.LightGray,
-                    modifier = Modifier.padding(start = 13.dp)
-                )
-            }
 
-            Button(
-                onClick = { vibratePhone(context) },
-                colors = ButtonDefaults.buttonColors(Color(0xff343434)),
-                modifier = Modifier
-                    .width(160.dp)
-                    .height(50.dp)
-                    .offset(x = 45.dp)
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_rocket_launch_24),
-                    contentDescription = "Ícone de enviar email",
+                Row(
                     modifier = Modifier
-                        .size(30.dp),
-                    tint = Color.LightGray
+                        .fillMaxWidth()
+                        .padding(top = 15.dp),
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalArrangement = Arrangement.SpaceEvenly
 
-                )
-                Text(
-                    text = "Enviar",
-                    fontSize = 20.sp,
-                    color = Color.LightGray,
-                    modifier = Modifier.padding(start = 13.dp)
-                )
+                ) {
+                    Button(
+                        onClick = { vibratePhone(context) },
+                        colors = ButtonDefaults.buttonColors(Color(0xff343434)),
+                        modifier = Modifier
+                            .width(160.dp)
+                            .height(50.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_settings_24),
+                            contentDescription = "Ícone de configurações",
+                            modifier = Modifier
+                                .size(30.dp),
+                            tint = Color.White
+
+                        )
+                        Text(
+                            text = "Opções",
+                            fontSize = 20.sp,
+                            color = Color.White,
+                            modifier = Modifier.padding(start = 13.dp)
+                        )
+                    }
+
+                    Button(
+                        onClick = { vibratePhone(context) },
+                        colors = ButtonDefaults.buttonColors(Color(0xff343434)),
+                        modifier = Modifier
+                            .width(160.dp)
+                            .height(50.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_rocket_launch_24),
+                            contentDescription = "Ícone de enviar email",
+                            modifier = Modifier
+                                .size(30.dp),
+                            tint = Color.White
+
+                        )
+                        Text(
+                            text = "Enviar",
+                            fontSize = 20.sp,
+                            color = Color.White,
+                            modifier = Modifier.padding(start = 13.dp)
+                        )
+                    }
+                }
             }
-        }
-
-
-
-
-
-
-
-
-
         }
     }
-
 }
 
 @Preview(showSystemUi = true, showBackground = true)
